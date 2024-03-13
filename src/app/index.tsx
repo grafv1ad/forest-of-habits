bc59b010c71b320c34fa521684cfa5f8f73c009e
Vladislav Petrov <grafv1ad@yandex.com>
2024-03-13 13:22:15 +0500

    add tailwind and setup base structure

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import logo from "../images/logo.svg";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
