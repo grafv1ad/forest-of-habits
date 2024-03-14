7af98cdcb6a6b8c525745288a06849d2d0fb5d5b
Vladislav Petrov <grafv1ad@yandex.com>
2024-03-14 18:05:13 +0500

    rename components with first uppercase letter

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
-import Content from "../components/content/index.tsx";
-import Footer from "../components/footer/index.tsx";
-import Header from "../components/header/index.tsx";
+import Content from "../components/Content/index.tsx";
+import Footer from "../components/Footer/index.tsx";
+import Header from "../components/Header/index.tsx";
 import "./index.css";
 
 function App() {
