99dc6ba66490cf26bc9092cad952c25854252c87
Vladislav Petrov <grafv1ad@yandex.com>
2024-03-19 13:05:35 +0500

    update checkbox component

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 import { CheckboxProps } from "types";
 
-const Checkbox: React.FC<CheckboxProps> = ({ extraClass, children }) => {
+const Checkbox: React.FC<CheckboxProps> = ({
+  checked = false,
+  children,
+  extraClass,
+}) => {
+  const [checkedStatus, setCheckedStatus] = useState<boolean>(checked);
+  const classes = classNames(
+    "relative flex items-center cursor-pointer",
+    extraClass
+  );
   return (
-    <label className={`relative flex items-center pl-6 ${extraClass}`}>
-      <input className="invisible w-10 h-10 peer" type="checkbox"></input>
-      <span
-        className={`absolute flex items-center justify-center w-10 h-10 bg-beige-600 left-0 rounded-lg peer-checked:before:content-checkbox peer-hover:opacity-70 peer-active:opacity-50`}
-      ></span>
-      {children}
+    <label className={classes}>
+      <input
+        className="invisible absolute pointer-events-none peer"
+        type="checkbox"
+        checked={checkedStatus}
+        onChange={() => setCheckedStatus(!checkedStatus)}
+      ></input>
+      <span className="flex shrink-0 items-center justify-center w-10 h-10 mr-3 bg-beige-600 left-0 rounded-lg border-2 before:flex before:content-checkbox before:opacity-0 before:transition-opacity before:duration-150 peer-checked:before:opacity-100 transition-colors duration-150 peer-hover:border-main peer-active:border-beige-600"></span>
+      <span className="leading-tight">{children}</span>
     </label>
   );
 };
--- src/types/checkbox.ts
+++ src/types/checkbox.ts
@@ -1,6 +1,7 @@
 import React from "react";
 
 export interface CheckboxProps {
+  checked?: boolean;
   children: React.ReactNode;
   extraClass?: string;
 }
