fcdb7977f38382c87df345ce411abea0e4d87133
Viktoria <vorobeva.vik.99@mail.ru>
2024-03-26 20:25:38 +0300

    add baseUrl

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 import { getCookie } from "./cookies";
 
 export const axiosInstance = axios.create({
+  baseURL: "http://127.0.0.1:8097",
   timeout: 3000,
   withCredentials: true,
   headers: { Authorization: `Bearer ${getCookie("token")}` },
