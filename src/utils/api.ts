cb7d84c1d87459ebc87b484bac2f52683dc01632
Vladislav Petrov <grafv1ad@yandex.com>
2024-04-08 16:29:45 +0500

    hotfix autorization

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,8 +2,17 @@ import axios from "axios";
 
 import { getCookie } from "./cookies";
 
-export const axiosInstance = axios.create({
+const axiosConfig = {
   baseURL: "http://127.0.0.1:8097",
   timeout: 3000,
-  headers: { Authorization: `Bearer ${getCookie("jwt-token")}` },
-});
+  headers: {},
+};
+
+const token = getCookie("jwt-token");
+if (token) {
+  axiosConfig.headers = {
+    Authorization: `Bearer ${token}`,
+  };
+}
+
+export const axiosInstance = axios.create(axiosConfig);
