47a5e86589bd869e530233d86c9fbd001cf89b89
Vladislav Petrov <grafv1ad@yandex.com>
2024-05-23 14:28:34 +0500

    replaced css bg by img

--- src/components/HeaderUser/index.tsx
+++ src/components/HeaderUser/index.tsx
@@ -11,14 +11,13 @@ const HeaderUser = () => {
       {isLoaded && <div>{isAuth ? username : "Войти"}</div>}
       <div className="w-8 h-8 rounded-full bg-beige-600 border border-main relative flex items-center justify-center overflow-hidden">
         {emailHash && (
-          <div
-            className="w-full h-full bg-beige-600 bg-[image:var(--avatar-url)] bg-no-repeat bg-center bg-contain z-[2]"
-            style={
-              {
-                "--avatar-url": `url('https://gravatar.com/avatar/${emailHash}?d=robohash&s=100')`,
-              } as React.CSSProperties
-            }
-          ></div>
+          <div className="w-full h-full bg-beige-600 bg-no-repeat bg-center bg-contain z-[2]">
+            <img
+              src={`https://gravatar.com/avatar/${emailHash}?d=robohash&s=100`}
+              className="w-full h-full object-contain"
+              alt=""
+            />
+          </div>
         )}
         <div className="svg-wrapper w-8/12 aspect-square absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
           <UserSVG />
